819b3b2d431dab5e61e7065eb40443769ce8f947
Ann Chen <annchen@andrew.cmu.edu>
2016-02-17 12:51:29 -0500

    made changes to articles

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+	belongs_to :category
+	validates_presence_of :title, :content
+	scope :active, where('active = ?', true)
+  	scope :alphabetical, order('title')
+
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :photos
-  
+   has_many :articles
+
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
 end
