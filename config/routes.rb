7f8449f3074cbe12dbadb8b1533237ccb09ea31f
Ann Chen <annchen@andrew.cmu.edu>
2016-02-17 19:11:16 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ KcwWorld::Application.routes.draw do
   resources :categories
   resources :proverbs
   resources :photos
-    
+  resources :articles 
+
+  
   get 'home' => 'home#index', :as => :home
   root :to => 'home#index'
 
